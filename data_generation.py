cb4b340588d4650e6d2d5f602c183c95078126f0
srayan00 <srayan@umich.edu>
2025-08-20 13:35:59 -0400

    Generated data

---
+++ data_generation.py
@@ -0,0 +1,105 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+
+
+class GaussianRandomField:
+
+    def __init__(self, num_samples, dim, alpha, beta, gamma, device, seed=None):
+        if num_samples % 2 == 0:
+            raise ValueError("Number of samples must be odd")
+        self.k_max = num_samples // 2
+        self.dim = dim
+        self.alpha = alpha
+        self.beta = beta
+        self.gamma = gamma
+        self.device = device if device else torch.cpu()
+        self.num_samples = num_samples
+
+
+        if seed is not None:
+            np.random.seed(seed)
+            torch.manual_seed(seed)
+        if dim == 1:
+            k_range = torch.arange(0, self.k_max + 1)
+            k_range_negative = torch.arange(-self.k_max, 0)
+            self.krange = torch.concatenate((k_range, k_range_negative))
+            self.psd = self._compute_psd_1d(self.krange)
+        elif dim == 2:
+            kx = torch.arange(0, self.k_max + 1)
+            k_range_negative = torch.arange(-self.k_max, 0)
+            kx = torch.concatenate((kx, k_range_negative))
+            ky = torch.arange(0, self.k_max + 1)
+            self.kx, self.ky = torch.meshgrid(kx, ky, indexing = "ij")
+            self.psd = self._compute_psd_2d(self.kx, self.ky)
+        else:
+            raise ValueError("Dimension must be either 1 or 2.")
+
+    def _compute_psd_1d(self, krange):
+        psd = (self.alpha ** (1/2)) * (4 * np.pi**2 * krange**2 + self.beta)**(-self.gamma/2)
+        return psd
+    
+    def _compute_psd_2d(self, kx, ky):
+        psd = (self.alpha ** (1/2)) * (4 * np.pi**2 * (kx**2 + ky**2) + self.beta)**(-self.gamma/2)
+        return psd
+    
+    def generate(self, n_samples):
+        if self.dim == 1:
+            return self._generate_1d(n_samples)
+        elif self.dim == 2:
+            return self._generate_2d(n_samples)
+        else:
+            raise NotImplementedError("2D Gaussian random field generation is not implemented yet.")
+    
+    def _generate_1d(self, n_samples):
+        psd = self.psd
+        real_positive_freq = torch.randn(n_samples, self.k_max + 1) # Real-valued part
+        complex_positive_freq = torch.randn(n_samples, self.k_max + 1) # complex-valued part
+        positive_freq = real_positive_freq + 1.j *complex_positive_freq # Zs for positive frequency
+        conjugate_positive_freq = np.conjugate(positive_freq) # conjugate frequencies
+        freq = torch.concatenate((positive_freq, torch.flip(conjugate_positive_freq, dims = [1])[:, :-1]), axis = 1)  # f^*(k) = f(-k)
+        freq[:, 0] = torch.randn(n_samples)
+        fourier_coeffs = psd * freq
+        field = torch.fft.ifft(fourier_coeffs, n= self.num_samples, norm = "ortho")
+        return field.real
+    
+    def visualize(self):
+        samples = self.generate(10)
+        sharey = True if self.dim == 2 else False
+        fig, ax = plt.subplots(2, 5, sharey = sharey)
+        for i in range(2):
+            for j in range(5):
+                idx = 2*i + j
+                curr = samples[idx]
+                if self.dim == 1:
+                    ax[i][j].plot(np.linspace(0, 1, self.num_samples), curr)
+                else:
+                    kx = np.linspace(0, 1, self.num_samples)
+                    ky = np.linspace(0, 1, self.num_samples)
+                    kx, ky = np.meshgrid(kx, ky)
+                    z = ax[i][j].pcolormesh(kx, ky, curr)
+                    plt.colorbar(z, ax=ax[i][j])
+                ax[i][j].set_title(f"Sample {idx}")
+        plt.show()
+        
+
+
+
+    # def _generate_1d(self, n_samples):
+    #     zs = torch.random.normal(size = (n_samples, self.k_max + 1)) + 1.j * torch.random.normal(size = (n_samples, self.k_max + 1))
+    #     zs[:, 0] = torch.randn(n_samples)
+    #     coefs = self.psd * zs
+    #     field = torch.fft.irfft(coefs, n = self.num_samples, norm= "ortho")
+
+    def _generate_2d(self, n_samples):
+        psd = self.psd
+        real_positive_freq = torch.randn(n_samples, 2*self.k_max + 1, self.k_max + 1) # Real-valued part
+        complex_positive_freq = torch.randn(n_samples, 2*self.k_max + 1, self.k_max + 1)
+        hermitian_half = real_positive_freq + 1.j *complex_positive_freq
+        hermitian_half[:, 0, 0] = torch.randn(n_samples)
+        fourier_coefs = psd * hermitian_half
+        return torch.fft.irfft2(fourier_coefs, s= (self.num_samples, self.num_samples), norm = "ortho")
+
+
+    
+    
--- numerical_solver.py
+++ numerical_solver.py
@@ -48,17 +48,33 @@ class MultigridSolver(NumericalSolver):
     def __init__(self, equation: PDE, levels=3):
         super().__init__(equation)
         self.levels = levels
+        self.equations = [self.equation]
         self.restrictor, self.interpolator = self.build_restrictor_interpolator()
-    
+        self.restrictors = []
+        self.interpolators = []
+        for i in range(levels - 1):
+            restrictor, interpolator = self.build_restrictor_interpolator()
+            self.restrictors.append(restrictor)
+            self.interpolators.append(interpolator)
+            if self.equation.dimension == 1:
+                new_equation = self.equation.__class__(self.equation.a_func, self.equation.f_func, self.equation.boundary,
+                                                    self.equation.x[::2], A=self.build_coefficient_matrix(self.equation, restrictor, interpolator))
+            else:
+                new_equation = self.equation.__class__(self.equation.a_func, self.equation.f_func, self.equation.boundary,
+                                                    self.equation.x[::2], self.equation.y[::2], A=self.build_coefficient_matrix(self.equation, restrictor, interpolator))
+            self.equations.append(new_equation)
+
     def index(self, i, j, len_y=None):
         if len_y is None:
             len_y = len(self.equation.y)
         return i * len_y + j if self.equation.dimension == 2 else i
 
-    def build_restrictor_interpolator_dirichlet(self):
+    def build_restrictor_interpolator_dirichlet(self, curr_equation=None):
         # Apply a simple full weighting restriction for 1d
-        if self.equation.dimension == 1:
-            n_h = len(self.equation.x) - 2 # Exclude boundary points
+        if curr_equation is None:
+            curr_equation = self.equation
+        if curr_equation.dimension == 1:
+            n_h = len(curr_equation.x) - 2 # Exclude boundary points
             # n_2h = (n_h + 1) // 2 # include border points
              # R[0, 0] = 1.0
             # R[-1, -1] = 1.0
@@ -84,7 +100,7 @@ class MultigridSolver(NumericalSolver):
                 I[j + 1, i] = 1.0
                 I[j + 2, i] = 0.5
         else:
-            n_hx, n_hy = len(self.equation.x) - 2, len(self.equation.y) - 2 # Exclude boundary points
+            n_hx, n_hy = len(curr_equation.x) - 2, len(curr_equation.y) - 2 # Exclude boundary points
             n_h = n_hx * n_hy
             n_2hx, n_2hy = n_hx // 2, n_hy // 2
             n_2h = n_2hx * n_2hy
@@ -118,10 +134,13 @@ class MultigridSolver(NumericalSolver):
                     I[self.index(2 * i, 2 * j, n_hy), idx_2h] = 0.25
         return R, I
     
-    def build_restrictor_interpolator_periodic(self):
+    def build_restrictor_interpolator_periodic(self, curr_equation=None):
         # Apply a simple full weighting restriction for 1d
-        if self.equation.dimension == 1:
-            n_h = len(self.equation.x)
+        if curr_equation is None:
+            curr_equation = self.equation
+        # Apply a simple full weighting restriction for 1d
+        if curr_equation.dimension == 1:
+            n_h = len(curr_equation.x)
             n_2h = (n_h + 1) // 2 # include border points
             R = np.zeros((n_2h, n_h))
             I = np.zeros((n_h, n_2h))
@@ -136,7 +155,7 @@ class MultigridSolver(NumericalSolver):
                 I[j, i] = 1.0
                 I[(j + 1 + n_h) % n_h, i] = 0.5
         else:
-            n_hx, n_hy = len(self.equation.x), len(self.equation.y)
+            n_hx, n_hy = len(curr_equation.x), len(curr_equation.y)
             n_h = n_hx * n_hy
             n_2hx, n_2hy = (n_hx + 1) // 2, (n_hy + 1) // 2
             n_2h = n_2hx * n_2hy
@@ -170,76 +189,133 @@ class MultigridSolver(NumericalSolver):
                     I[self.index((2 * i + 1) % n_hx, (2 * j + 1) % n_hy, n_hy), idx_2h] = 0.25
         return R, I
 
-    def build_restrictor_interpolator(self):
+    def build_restrictor_interpolator(self, curr_equation=None):
         if self.equation.boundary == 'Dirichlet':
-            restrictor, interpolator = self.build_restrictor_interpolator_dirichlet()
+            restrictor, interpolator = self.build_restrictor_interpolator_dirichlet(curr_equation)
         elif self.equation.boundary == 'Periodic':
-            restrictor, interpolator = self.build_restrictor_interpolator_periodic()
+            restrictor, interpolator = self.build_restrictor_interpolator_periodic(curr_equation)
         else:
             raise ValueError("Boundary condition must be either 'Dirichlet' or 'Periodic'")
         return restrictor, interpolator
     
-    def restrict(self, u):
+    def build_coefficient_matrix_periodic(self, curr_equation=None, restrictor=None, interpolator=None):
+        if curr_equation is None:
+            curr_equation = self.equation
+        if restrictor is None or interpolator is None:
+            restrictor, interpolator = self.build_restrictor_interpolator(curr_equation)
+        new_A = curr_equation.A.copy()
+        return restrictor @ new_A @ interpolator
+    
+    def build_coefficient_matrix_dirichlet(self, curr_equation=None, restrictor=None, interpolator=None):
+        if curr_equation.dimension == 1:
+            new_A = curr_equation.A[1:-1, 1:-1].copy()  # Exclude Dirichlet boundaries
+            new_A = restrictor @ new_A @ interpolator
+            # include dirichlet boundaries
+            new_A = np.pad(new_A, ((1, 1), (1, 1)), mode='constant', constant_values=0)
+            new_A[0, 0] = 1.0
+            new_A[-1, -1] = 1.0
+        else:
+            new_A = curr_equation.A.reshape((len(curr_equation.x), len(curr_equation.y), len(curr_equation.x), len(curr_equation.y)))[1:-1, 1:-1, 1:-1, 1:-1].copy()  # Exclude Dirichlet boundaries
+            new_len_x = len(curr_equation.x) - 2
+            new_len_y = len(curr_equation.y) - 2
+            new_A = new_A.reshape((new_len_x * new_len_y, new_len_x * new_len_y))
+            new_A = restrictor @ new_A @ interpolator
+            new_A = new_A.reshape((new_len_x // 2, new_len_y // 2, new_len_x // 2, new_len_y // 2))
+            new_A = np.pad(new_A, ((1, 1), (1, 1), (1, 1), (1, 1)), mode='constant', constant_values=0)
+            for i in range((new_len_x // 2) + 2):
+                new_A[i, 0, i, 0] = 1.0
+                new_A[i, -1, i, -1] = 1.0
+            for j in range((new_len_y // 2) + 2):
+                new_A[0, j, 0, j] = 1.0
+                new_A[-1, j, -1, j] = 1.0
+            len_x, len_y = new_A.shape[0], new_A.shape[1]
+            new_A = new_A.reshape((len_x * len_y, len_x * len_y))
+        return new_A
+    
+    def build_coefficient_matrix(self, curr_equation=None, restrictor=None, interpolator=None):
+        if curr_equation is None:
+            curr_equation = self.equation
         if self.equation.boundary == 'Dirichlet':
-            if self.equation.dimension == 1:
-                ans = self.restrictor @ u[1:-1]
+            return self.build_coefficient_matrix_dirichlet(curr_equation, restrictor, interpolator)
+        elif self.equation.boundary == 'Periodic':
+            return self.build_coefficient_matrix_periodic(curr_equation, restrictor, interpolator)
+        else:
+            raise ValueError("Boundary condition must be either 'Dirichlet' or 'Periodic'")
+
+    def restrict(self, u, curr_equation=None, restrictor=None):
+        if restrictor is None:
+            restrictor = self.restrictor
+        if curr_equation is None:
+            curr_equation = self.equation
+        if curr_equation.boundary == 'Dirichlet':
+            if curr_equation.dimension == 1:
+                ans = restrictor @ u[1:-1]
                 return np.concatenate(([0], ans, [0]))  # Add Dirichlet boundary conditions
             else:
-                len_x = len(self.equation.x)
-                len_y = len(self.equation.y)
-                ans = self.restrictor @ u.reshape((len_x, len_y))[1:-1, 1:-1].flatten()
+                len_x = len(curr_equation.x)
+                len_y = len(curr_equation.y)
+                ans = restrictor @ u.reshape((len_x, len_y))[1:-1, 1:-1].flatten()
                 new_len_x = (len_x - 2)// 2
                 new_len_y = (len_y - 2)// 2
                 return np.pad(ans.reshape((new_len_x, new_len_y)), ((1, 1), (1, 1)), mode='constant', constant_values=0).flatten()  # Add Dirichlet boundary conditions
 
-        return self.restrictor @ u
-
-    def prolong(self, u_coarse):
-        if self.equation.boundary == 'Dirichlet':
-            if self.equation.dimension == 1:
-                ans = self.interpolator @ u_coarse[1:-1]
+        return restrictor @ u
+    
+    def prolong(self, u_coarse, curr_equation=None, interpolator=None):
+        if interpolator is None:
+            interpolator = self.interpolator
+        if curr_equation is None:
+            curr_equation = self.equation
+        if curr_equation.boundary == 'Dirichlet':
+            if curr_equation.dimension == 1:
+                ans = interpolator @ u_coarse[1:-1]
                 return np.concatenate(([0], ans, [0]))  # Add Dirichlet boundary conditions
             else:
-                len_x = (len(self.equation.x) - 2)
-                len_y = (len(self.equation.y) - 2)
-                ans = self.interpolator @ u_coarse.reshape((len_x // 2 + 2, len_y // 2 + 2))[1:-1, 1:-1].flatten()
+                len_x = (len(curr_equation.x) - 2)
+                len_y = (len(curr_equation.y) - 2)
+                ans = interpolator @ u_coarse.reshape((len_x // 2 + 2, len_y // 2 + 2))[1:-1, 1:-1].flatten()
                 return np.pad(ans.reshape((len_x, len_y)), ((1, 1), (1, 1)), mode='constant', constant_values=0).flatten()  # Add Dirichlet boundary conditions
-        return self.interpolator @ u_coarse
+        return interpolator @ u_coarse
     
     def valid_level(self, n):
         return n > 3
 
 
-    def iteration(self, u_old):
+    def iteration(self, u_old, curr_equation=None, level = 0):
+        if curr_equation is None:
+            curr_equation = self.equation
+            restrictor, interpolator = self.restrictor, self.interpolator
+        else:
+            restrictor, interpolator = self.build_restrictor_interpolator(curr_equation)
         # Pre-smoothing
         print("Pre-smoothing")
-        u = WeightedJacobiSolver(self.equation, weight=0.8).solve(u_init=u_old, max_iter=3)
+        u = WeightedJacobiSolver(curr_equation, weight=0.8).solve(u_init=u_old, max_iter=3)
         # Compute residual
         print("Computing residual")
-        r = self.equation.b - self.equation.A @ u
+        r = curr_equation.b - curr_equation.A @ u
 
         # Restrict residual to coarser grid
         print("Restricting residual")
-        r_coarse = self.restrict(r)
+        r_coarse = self.restrict(r, curr_equation, restrictor)
 
         u_init = np.zeros_like(r_coarse)
 
         # Solver on coarser grid
         print("Solving on coarser grid")
-        if self.equation.boundary == 'Dirichlet':
-            if self.equation.dimension == 1:
-                new_A = self.equation.A[1:-1, 1:-1].copy()  # Exclude Dirichlet boundaries
-                new_A = self.restrictor @ new_A @ self.interpolator
+        if curr_equation.boundary == 'Dirichlet':
+            if curr_equation.dimension == 1:
+                new_A = curr_equation.A[1:-1, 1:-1].copy()  # Exclude Dirichlet boundaries
+                new_A = restrictor @ new_A @ interpolator
                 # include dirichlet boundaries
                 new_A = np.pad(new_A, ((1, 1), (1, 1)), mode='constant', constant_values=0)
                 new_A[0, 0] = 1.0
                 new_A[-1, -1] = 1.0
             else:
-                new_A = self.equation.A.reshape((len(self.equation.x), len(self.equation.y), len(self.equation.x), len(self.equation.y)))[1:-1, 1:-1, 1:-1, 1:-1].copy()  # Exclude Dirichlet boundaries
-                new_len_x = len(self.equation.x) - 2
-                new_len_y = len(self.equation.y) - 2
+                new_A = curr_equation.A.reshape((len(curr_equation.x), len(curr_equation.y), len(curr_equation.x), len(curr_equation.y)))[1:-1, 1:-1, 1:-1, 1:-1].copy()  # Exclude Dirichlet boundaries
+                new_len_x = len(curr_equation.x) - 2
+                new_len_y = len(curr_equation.y) - 2
                 new_A = new_A.reshape((new_len_x * new_len_y, new_len_x * new_len_y))
-                new_A = self.restrictor @ new_A @ self.interpolator
+                new_A = restrictor @ new_A @ interpolator
                 new_A = new_A.reshape((new_len_x // 2, new_len_y // 2, new_len_x // 2, new_len_y // 2))
                 new_A = np.pad(new_A, ((1, 1), (1, 1), (1, 1), (1, 1)), mode='constant', constant_values=0)
                 for i in range((new_len_x // 2) + 2):
@@ -251,13 +327,13 @@ class MultigridSolver(NumericalSolver):
                 len_x, len_y = new_A.shape[0], new_A.shape[1]
                 new_A = new_A.reshape((len_x * len_y, len_x * len_y))
         else:
-            new_A = self.equation.A.copy()
-            new_A = self.restrictor @ new_A @ self.interpolator
-        
-        if self.equation.dimension == 1:
-            equation_coarse =  self.equation.__class__(a_func=None, f_func=r_coarse, boundary=self.equation.boundary, x=self.equation.x[::2], A=new_A)
+            new_A = curr_equation.A.copy()
+            new_A = restrictor @ new_A @ interpolator
+
+        if curr_equation.dimension == 1:
+            equation_coarse =  curr_equation.__class__(a_func=None, f_func=r_coarse, boundary=curr_equation.boundary, x=curr_equation.x[::2], A=new_A)
         else:
-            equation_coarse =  self.equation.__class__(None, r_coarse, self.equation.boundary, self.equation.x[::2], self.equation.y[::2], A=new_A)
+            equation_coarse =  curr_equation.__class__(None, r_coarse, curr_equation.boundary, curr_equation.x[::2], curr_equation.y[::2], A=new_A)
 
         # equation_coarse = self.equation.__class__(None, r_coarse, self.equation.boundary, self.equation.x[::2], self.equation.y[::2] if self.equation.dimension == 2 else None, A=new_A)
         # equation_coarse = PoissonEquation2D(None, r_coarse, self.equation.boundary, self.equation.x[::2], self.equation.y[::2], A=new_A)
@@ -266,7 +342,7 @@ class MultigridSolver(NumericalSolver):
 
         # Prolongate solution to fine grid
         print("Prolongating solution to fine grid")
-        u_prolonged = self.prolong(u_coarse)
+        u_prolonged = self.prolong(u_coarse, curr_equation, interpolator)
 
         # Update solution
         print("Updating solution")
@@ -274,6 +350,6 @@ class MultigridSolver(NumericalSolver):
 
         # Post-smoothing
         print("Post-smoothing")
-        u = WeightedJacobiSolver(self.equation, weight=0.8).solve(u_init=u, max_iter=3)
+        u = WeightedJacobiSolver(curr_equation, weight=0.8).solve(u_init=u, max_iter=3)
         return u
 
